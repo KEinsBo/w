d9252f8c5a023c45e427e20649f03210cc15f2cf
KEinsBo <mattis-richter@gmx.de>
2025-03-15 19:23:27 +0100

    index.js removal

--- Testprojekt 1/index.js
+++ Testprojekt 1/index.js
@@ -1,21 +1,3 @@
-
-
-function readTextFile(file) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function () {
-      if(rawFile.readyState === 4)  {
-        if(rawFile.status === 200 || rawFile.status == 0) {
-          var allText = rawFile.responseText;
-          console.log(allText);
-         }
-      }
-    }
-    rawFile.send(null);
-  }
-
 window.alert("hi");
 
 const checkbox = document.getElementById("checky");
-
-window.alert(readTextFile("file://C:/Code/Git hub/fake werwolf/w/Testprojekt 1/checkbox.log"));
